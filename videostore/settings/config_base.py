044f035e79c675f30a547e9cf65af1b934210c24
Tomislav Adamic <tomislav@sedmiodjel.com>
2016-11-30 09:36:58 +0100

    Logging config

--- videostore/settings/config_base.py
+++ videostore/settings/config_base.py
@@ -1,11 +1,31 @@
+import abc
 import configparser
+import glob
+import logging
+import logging.handlers
 import os
+import shutil
+import socket
+from datetime import datetime
 
+import pytz
+import tzlocal
 from unipath import Path
 
 
 class ImproperlyConfiguredError(RuntimeError):
     pass
+
+
+class ContextFilter(logging.Filter):
+    HOSTNAME = socket.gethostname()
+
+    def filter(self, record):
+        record.hostname = ContextFilter.HOSTNAME
+        record.isotime_utc = datetime.fromtimestamp(
+            record.created, tzlocal.get_localzone()
+        ).astimezone(pytz.utc).isoformat()
+        return True
 
 
 class ConfigBase:
@@ -40,9 +60,27 @@ class ConfigBase:
     }
 
     # --------------------------------------------------------------------------
-    # Flask-SQWLAlchemy config
+    # Flask-SQLAlchemy config
     # --------------------------------------------------------------------------
     SQLALCHEMY_DATABASE_URI = None
+
+    # --------------------------------------------------------------------------
+    # Logging config
+    # --------------------------------------------------------------------------
+    FILELOG_ENABLED = True
+    FILELOG_LOGLEVEL = None
+    SYSLOG_LOGLEVEL = None
+    SYSLOG_ADDRESS = None
+    SYSLOG_FACILITY = None
+    SYSLOG_SOCKET_TYPE = None
+    FILELOG_FORMAT = u'[%(asctime)s] [PID: %(process)s] [{0}] %(levelname)s: %(message)s'.format(APPLICATION_NAME)
+    SYSLOG_FORMAT = '%(isotime_utc)s %(hostname)s {0}[%(process)d]: %(levelname)s %(message)s'.format(APPLICATION_NAME)
+    ADDITIONAL_LOGGERS = [
+        'alembic',
+        'werkzeug',
+        'sqlalchemy',
+        'sqlalchemy.engine',
+    ]
 
     def read_config_file(self):
         config_parser = configparser.ConfigParser()
@@ -53,9 +91,14 @@ class ConfigBase:
         self.SECRET_KEY = section.get('secret_key', fallback='')
 
         section = config_parser['Database']
-        self.SQLALCHEMY_DATABASE_URI = section.get(
-            'sqlalchemy_uri', fallback=''
-        )
+        self.SQLALCHEMY_DATABASE_URI = section.get('sqlalchemy_uri', fallback='')
+
+        section = config_parser['Logging']
+        self.FILELOG_LOGLEVEL = section.get('filelog_loglevel', fallback='debug')
+        self.SYSLOG_LOGLEVEL = section.get('syslog_loglevel', fallback='debug')
+        self.SYSLOG_ADDRESS = section.get('syslog_address', fallback=None)
+        self.SYSLOG_FACILITY = section.get('syslog_facility', fallback='local1')
+        self.SYSLOG_SOCKET_TYPE = section.get('syslog_socket_type', fallback='UDP')
 
     def validate(self):
         errors = dict()
@@ -66,8 +109,81 @@ class ConfigBase:
         if errors:
             raise ImproperlyConfiguredError(errors)
 
+    @property
+    def file_log_path(self):
+        return self.APPLICATION_REPO_ROOT.child('log').child(
+            type(self).__name__.lower() + '.log'
+        )
+
+    def file_log_handler(self, file_path):
+        handler = logging.handlers.WatchedFileHandler(
+            filename=(file_path or self.file_log_path)
+        )
+        handler.setFormatter(logging.Formatter(self.FILELOG_FORMAT))
+        handler.setLevel({'info': logging.INFO,
+                          'critical': logging.CRITICAL,
+                          'warning': logging.WARNING,
+                          'debug': logging.DEBUG,
+                          'error': logging.ERROR,}[self.FILELOG_LOGLEVEL.lower()])
+        return handler
+
+    def syslog_log_handler(self):
+        handler = logging.NullHandler()
+        if self.SYSLOG_ADDRESS:
+            if ':' in self.SYSLOG_ADDRESS:
+                ip, port = self.SYSLOG_ADDRESS.split(':')
+                syslog_address = tuple([ip, int(port)])
+            else:
+                syslog_address = self.SYSLOG_ADDRESS
+
+            handler = logging.handlers.SysLogHandler(
+                address=syslog_address, facility=self.SYSLOG_FACILITY,
+                socktype={'tcp': socket.SOCK_STREAM,
+                          'udp': socket.SOCK_DGRAM,}[self.SYSLOG_SOCKET_TYPE.lower()]
+            )
+            handler.setFormatter(logging.Formatter(self.SYSLOG_FORMAT))
+
+        handler.setLevel({'info': logging.INFO,
+                          'critical': logging.CRITICAL,
+                          'warning': logging.WARNING,
+                          'debug': logging.DEBUG,
+                          'error': logging.ERROR,}[self.SYSLOG_LOGLEVEL.lower()])
+
+        handler.addFilter(ContextFilter())
+
+        return handler
+
+    def _cleanup_logger_handlers(self, logger):
+        while logger.handlers:
+            logger.removeHandler(logger.handlers[0])
+
+    def _init_loggers(self, app):
+        # Our own handlers
+        handlers = []
+        if self.FILELOG_ENABLED:
+            handlers.append(self.file_log_handler(file_path=self.file_log_path))
+        if self.SYSLOG_ADDRESS:
+            handlers.append(self.syslog_log_handler())
+
+        # loggers we are interested in
+        loggers = set(
+            [app.logger] + [
+                logging.getLogger(logger_name)
+                for logger_name in self.ADDITIONAL_LOGGERS
+            ]
+        )
+
+        for logger in loggers:
+            self._cleanup_logger_handlers(logger)
+            # logger should pass all messages to handler, and handlers' log
+            # level then decides what will be logged.
+            logger.setLevel(logging.DEBUG)
+            for handler in handlers:
+                logger.addHandler(handler)
+
     def init_app(self, app):
         if app:
             self.read_config_file()
             self.validate()
             app.config.from_object(self)
+            self._init_loggers(app)
